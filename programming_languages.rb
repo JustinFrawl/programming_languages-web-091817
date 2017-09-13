c8d0c1e62a30c6479b03165de52060154f1a4ff9
Justin Frawley <freegamespeech@gmail.com>
2017-09-13 16:26:32 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
-def reformat_languages(languages)
+
   # your code here
-end
+  def reformat_languages(languages)
+    language_attributes = {}
+
+    languages.each do |functional, language_hash|
+
+      language_hash.each do |language, attribute_hash|
+        attribute_hash.each do |attribute, str_value|
+          if language_attributes[language].nil?
+            language_attributes[language] = {}
+          end
+          language_attributes[language][:style] ||= []
+          language_attributes[language][:style] << functional
+          if language_attributes[language][attribute].nil?
+            language_attributes[language][attribute] = str_value
+          end
+        end
+      end
+    end
+    language_attributes
+  end
